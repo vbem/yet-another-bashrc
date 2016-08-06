4f409c67dbd55fb2cf49a3087254a6b6bb2cf811
vbem <i@lilei.tech>
2016-08-06 20:52:41 +0800

    add pip bash completion support

--- bashrc.sh
+++ bashrc.sh
@@ -25,14 +25,22 @@ alias .curl.bench.teddysun='curl https://raw.githubusercontent.com/teddysun/acro
 alias .curl.bench.freevps='curl https://freevps.us/downloads/bench.sh | bash'
 alias .curl.ip='curl -s -4 icanhazip.com'
 alias .git.log='git log --graph --all --decorate --oneline'
+alias .python.unittest='python -m unittest -c -v'
+alias .python.unittest.discover='python -m unittest discover -c -v'
 alias .pip.install='pip install -i https://pypi.tuna.tsinghua.edu.cn/simple'
-alias .py.ut='python -m unittest -c -v'
-alias .py.ut.discover='python -m unittest discover -c -v'
 
-# functions
+# pyvenv & virtualenv
 function .source.activate {
     source $1/bin/activate
 }
+
+# pip bash completion
+_pip_completion() {
+    COMPREPLY=( $( COMP_WORDS="${COMP_WORDS[*]}" \
+                   COMP_CWORD=$COMP_CWORD \
+                   PIP_AUTO_COMPLETE=1 $1 ) )
+}
+complete -o default -F _pip_completion pip
 
 # colorful manpage
 export LESS_TERMCAP_mb=$'\E[01;31m'
@@ -57,24 +65,24 @@ PS1_SHLVL=$CLR_BEG'43;1'$CLR_MID'$(if [ 1 -ne $SHLVL ]; then echo " \\$SHLVL=$SH
 PS1_LOGIN=$CLR_BEG'43;1'$CLR_MID'$(shopt -q login_shell; if [ 0 -ne $? ]; then echo " non-login-shell "; fi)'$CLR_END
 PS1_GRP=$CLR_BEG'43;1'$CLR_MID'$(if [ "$(id -ng)" != "$(id -nu)" ]; then echo " effective-group:$(id -ng) "; fi)'$CLR_END
 # development environments
-PS1_PYVE=$CLR_BEG'44;1'$CLR_MID'$(if [ -n "$VIRTUAL_ENV" ]; then echo " py@$VIRTUAL_ENV ";fi)'$CLR_END
+VIRTUAL_ENV_DISABLE_PROMPT=1
+PS1_PYVENV=$CLR_BEG'44;1'$CLR_MID'$(if [ -n "$VIRTUAL_ENV" ]; then echo " pyvenv:$VIRTUAL_ENV ";fi)'$CLR_END
 GIT_PMT_LIST=(
     '/usr/share/git-core/contrib/completion/git-prompt.sh'
 )
 for nIndex in ${!GIT_PMT_LIST[@]}; do \
     if [ -f ${GIT_PMT_LIST[$nIndex]} ]; then \
+        GIT_PS1_SHOWDIRTYSTATE=1 \
+        GIT_PS1_SHOWSTASHSTATE=1 \
+        GIT_PS1_SHOWUNTRACKEDFILES=1 \
+        GIT_PS1_SHOWUPSTREAM="verbose legacy git" \
+        GIT_PS1_DESCRIBE_STYLE=branch \
+        GIT_PS1_SHOWCOLORHINTS=1 \
         source ${GIT_PMT_LIST[$nIndex]}; \
         PS1_GIT=$CLR_BEG'45;1'$CLR_MID'$(__git_ps1 " %s ")'$CLR_END; \
         break; \
     fi \
 done
 # integration
-PS1=$PS1_RET$PS1_LOC$PS1_SHLVL$PS1_LOGIN$PS1_GRP$PS1_PYVE$PS1_GIT$PS1_PMT
-#unset GIT_PMT_LIST CLR_BEG CLR_MID CLR_END PS1_RET PS1_LOC PS1_PMT PS1_SHLVL PS1_LOGIN PS1_GRP PS1_PYVE PS1_GIT
-VIRTUAL_ENV_DISABLE_PROMPT=1
-GIT_PS1_SHOWDIRTYSTATE=1
-GIT_PS1_SHOWSTASHSTATE=1
-GIT_PS1_SHOWUNTRACKEDFILES=1
-GIT_PS1_SHOWUPSTREAM="verbose legacy git"
-GIT_PS1_DESCRIBE_STYLE=branch
-GIT_PS1_SHOWCOLORHINTS=1
+PS1=$PS1_RET$PS1_LOC$PS1_SHLVL$PS1_LOGIN$PS1_GRP$PS1_PYVENV$PS1_GIT$PS1_PMT
+unset GIT_PMT_LIST CLR_BEG CLR_MID CLR_END PS1_RET PS1_LOC PS1_PMT PS1_SHLVL PS1_LOGIN PS1_GRP PS1_PYVENV PS1_GIT
