33dee45a3276dbd4d996e6ec09f587111d776e2c
Li Lei <i@lilei.tech>
2018-05-18 15:58:01 +0800

    Update bashrc-aliyun.sh

--- bashrc-aliyun.sh
+++ bashrc-aliyun.sh
@@ -1,5 +1,7 @@
 # aliases
 alias .aliyun='aliyun --mode EcsRamRole --ram-role-name $(curl -s 100.100.100.200/latest/meta-data/ram/security-credentials/) --region $(curl -s 100.100.100.200/latest/meta-data/region-id)'
+alias .ecs.role='curl -s 100.100.100.200/latest/meta-data/ram/security-credentials/'
+alias .ecs.role.sts='curl -s 100.100.100.200/latest/meta-data/ram/security-credentials/$(curl -s 100.100.100.200/latest/meta-data/ram/security-credentials/)'
 alias .ls='ls -alFh --time-style=long-iso --color=auto'
 alias .tree='tree -fiapughDFC --timefmt %F_%T --du --dirsfirst'
 alias .grep='grep -E -n --color=auto'
