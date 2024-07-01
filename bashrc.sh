86878e570be9adfbad648699f0c2a63194fbd0ae
vbem <i@lilei.tech>
2024-07-01 16:26:35 +0800

    refactor: Update bashrc.sh to fix PROMPT_COMMAND assignment
    The PROMPT_COMMAND assignment in bashrc.sh has been updated to include a check for the existence of the PROMPT_COMMAND variable before appending the history synchronization commands. This change ensures that the history synchronization commands are only added if the PROMPT_COMMAND variable is not empty.
    
    Refactor the assignment to:
    ```bash
    [[ -n "$PROMPT_COMMAND" ]] && export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"
    ```

--- bashrc.sh
+++ bashrc.sh
@@ -307,7 +307,7 @@ command -v hstr &>/dev/null && {
     export HISTFILESIZE=10000       # increase history file size (default is 500)
     export HISTSIZE=${HISTFILESIZE} # increase history size (default is 500)
     # ensure synchronization between bash memory and history file
-    export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"
+    [[ -n "$PROMPT_COMMAND" ]] && export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"
     if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hstr -- \C-j"'; fi
     # if this is interactive shell, then bind 'kill last command' to Ctrl-x k
     if [[ $- =~ .*i.* ]]; then bind '"\C-xk": "\C-a hstr -k \C-j"'; fi
